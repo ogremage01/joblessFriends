10286c233116534c856560a742643797ebc30726
jhs <skc05096@naver.com>
2025-05-09 16:11:45 +0900

    [공고] feat-jhs : 공고 단순리스트 생성완료 /  TABLE_CD / 채용공고 더미데이터 제작

--- 2_webProject/JobFinder/src/main/java/com/joblessfriend/jobfinder/recruitment/dao/RecruitmentDao.java
+++ 2_webProject/JobFinder/src/main/java/com/joblessfriend/jobfinder/recruitment/dao/RecruitmentDao.java
@@ -2,6 +2,7 @@ package com.joblessfriend.jobfinder.recruitment.dao;
 
 import com.joblessfriend.jobfinder.recruitment.domain.JobGroupVo;
 
+import com.joblessfriend.jobfinder.recruitment.domain.RecruitmentVo;
 import org.springframework.stereotype.Repository;
 
 
@@ -11,4 +12,5 @@ import java.util.List;
 public interface RecruitmentDao {
     List<JobGroupVo> jobGroupList();
     List<JobGroupVo> jobList(int jobGroupId);
+    public List<RecruitmentVo> recruitmentList();
 }
