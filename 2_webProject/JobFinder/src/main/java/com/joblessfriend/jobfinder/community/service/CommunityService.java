d8b46d044d184c9079ece8604953026d2e3c1509
yubeen <cath990409@gmail.com>
2025-05-09 17:38:43 +0900

    [게시판] feat-jyb: 게시판 글쓰기(파일업로드, 글쓰기) 중 '글쓰기'만 추가/게시물 약간 추가/mapper와 mybatis 게시글 맵핑 추가

--- 2_webProject/JobFinder/src/main/java/com/joblessfriend/jobfinder/community/service/CommunityService.java
+++ 2_webProject/JobFinder/src/main/java/com/joblessfriend/jobfinder/community/service/CommunityService.java
@@ -1,5 +1,13 @@
 package com.joblessfriend.jobfinder.community.service;
 
-public interface CommunityService {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import com.joblessfriend.jobfinder.community.domain.CommunityVo;
+
+public interface CommunityService{
+
+	void communityInsertOne(CommunityVo communityVo);
 
 }
