9d452f6b4701a7bd64eedb05552cfb642ee4624a
DESKTOP-07OJG0B\PC <fyuio2000@naver.com>
2025-05-09 11:01:56 +0900

    [이력서] cuh: 마이페이지 이력서관리 화면단 작업

--- 2_webProject/JobFinder/src/main/java/com/joblessfriend/jobfinder/resume/controller/ResumeController.java
+++ 2_webProject/JobFinder/src/main/java/com/joblessfriend/jobfinder/resume/controller/ResumeController.java
@@ -1,5 +1,18 @@
 package com.joblessfriend.jobfinder.resume.controller;
 
-public class ResumeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class ResumeController {
+	
+	@GetMapping("/home")
+	public String home() {
+		return "resume/resumeView";
+	}
+	
+	@GetMapping("/inventory")
+	public String inventory() {
+		return "resume/resumeManagementView";
+	}
 }
