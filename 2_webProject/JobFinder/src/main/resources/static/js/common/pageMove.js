a2e6742ac5421a6cb3b91935898bb24355363563
q1awse-a <iltor91@gmail.com>
2025-05-13 17:40:00 +0900

    [공통단] feat-jcm: 자동완성 기능 구현 중

--- 2_webProject/JobFinder/src/main/resources/static/js/common/pageMove.js
+++ 2_webProject/JobFinder/src/main/resources/static/js/common/pageMove.js
@@ -20,6 +20,25 @@ function pageMoveMainFnc() { location.href = '/'; }
 //채용정보
 	//목록
 	function pageMoveRecruitmentListFnc() { location.href = '/Recruitment/list'; }
+	//검색
+	function pageMoveSearchListFnc() {
+		const form = document.getElementById('searchForm');
+		
+		const queryParams = {
+		            page: (page) ? page : 1,
+		            recordSize: 8,
+		            pageSize: 10,
+		            keyword: form.keyword.value
+		        }
+
+	        /*
+	         *    location.pathname : 리스트 페이지의 URI("/Recruitment/list")를 의미
+	         *    new URLSearchParams(queryParams).toString() : queryParams의 모든 프로퍼티(key-value)를 쿼리 스트링으로 변환
+	         *    URI + 쿼리 스트링에 해당하는 주소로 이동
+	         *    (해당 함수가 리턴해주는 값을 브라우저 콘솔(console)에 찍어보시면 쉽게 이해하실 수 있습니다.)
+	         */
+	        location.href = location.pathname + '?' + new URLSearchParams(queryParams).toString();
+	}
 
 //관리자
 	//로그인
