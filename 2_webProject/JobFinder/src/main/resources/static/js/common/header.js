6912fbe99b047dd86b6c9af08e45a6929691505c
Chan242 <pcj5062@gmail.com>
2025-05-16 11:07:30 +0900

    [공통ui] pcj: 기업서비스 외부클릭 닫기 기능 추가, 헤더 카테고리(채용정보/커뮤니티) 스타일변화 css생성

--- 2_webProject/JobFinder/src/main/resources/static/js/common/header.js
+++ 2_webProject/JobFinder/src/main/resources/static/js/common/header.js
@@ -1,13 +1,21 @@
 
 var compServiceToggle = false;
 
-function companyPopup() {
-
+$('.corpNav').on('click', function(e){
+	
+	e.stopPropagation();
+	
 	compServiceToggle = !compServiceToggle;
+	
 	if(compServiceToggle){
 		$(".companyServiceNav").css("display","block");
 	}else{
 		$(".companyServiceNav").css("display","none");
 	}
 	
-}
+});
+
+//외부 클릭 시 닫기
+$(document).on('click', function () {
+    $('.companyServiceNav').css('display', 'none');
+});
