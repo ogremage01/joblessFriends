492de0e8764d00861f03d9b19e13b2c80c04d342
q1awse-a <iltor91@gmail.com>
2025-05-19 18:14:09 +0900

    [이력서] jcm: 자격증 추가, 삭제 기능 구현

--- 2_webProject/JobFinder/src/main/java/com/joblessfriend/jobfinder/resume/certificate/dao/CertificateDao.java
+++ 2_webProject/JobFinder/src/main/java/com/joblessfriend/jobfinder/resume/certificate/dao/CertificateDao.java
@@ -8,4 +8,8 @@ public interface CertificateDao {
 
 	List<CertificateVo> certificateSelectList(int resumeId);
 
+	void certificateInsertOne(CertificateVo certificateVo);
+
+	void certificateDeleteOne(int certificateId, int resumeId);
+
 }
