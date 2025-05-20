60e9b34cefe8547132779f0ab7fd96d492e0a045
q1awse-a <iltor91@gmail.com>
2025-05-20 18:03:54 +0900

    [이력서] feat-jcm: 이력서 교육 관련 부분 기능 구현CRD

---
+++ 2_webProject/JobFinder/src/main/java/com/joblessfriend/jobfinder/resume/service/EducationServiceImpl.java
@@ -0,0 +1,33 @@
+package com.joblessfriend.jobfinder.resume.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.joblessfriend.jobfinder.resume.dao.EducationDao;
+import com.joblessfriend.jobfinder.resume.domain.EducationVo;
+
+@Service
+public class EducationServiceImpl implements EducationService{
+
+	public EducationDao educationDao;
+	
+	@Override
+	public List<EducationVo> educationSelectList(int resumeId) {
+		// TODO Auto-generated method stub
+		return educationDao.educationSelectList(resumeId);
+	}
+
+	@Override
+	public void educationInsertOne(EducationVo educationVo) {
+		// TODO Auto-generated method stub
+		educationDao.educationInsertOne(educationVo);
+	}
+
+	@Override
+	public void educationDeleteOne(int eduId, int resumeId) {
+		// TODO Auto-generated method stub
+		educationDao.educationDeleteOne(eduId, resumeId);
+	}
+
+}
