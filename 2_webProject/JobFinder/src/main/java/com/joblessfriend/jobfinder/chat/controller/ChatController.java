abb92f8bbfe158b140aebda286092a7aefe4bd67
ogremage01 <curselife01@gmail.com>
2025-05-22 19:48:43 +0900

    [채팅]UI 구현 70%완료

--- 2_webProject/JobFinder/src/main/java/com/joblessfriend/jobfinder/chat/controller/ChatController.java
+++ 2_webProject/JobFinder/src/main/java/com/joblessfriend/jobfinder/chat/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.joblessfriend.jobfinder.chat.controller;
 
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,17 +10,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.joblessfriend.jobfinder.chat.domain.ChatRoomVo;
 import com.joblessfriend.jobfinder.chat.service.ChatService;
 
+@Controller
 public class ChatController {
 
 	
     private ChatService chatService;
 
-    @PostMapping("/rooms")
+    @PostMapping("chat/rooms")
     public ChatRoomVo createRoom(@RequestParam String name) {
        return chatService.createRoom(name);
     }
 
-    @GetMapping("/rooms")
+    @GetMapping("chat/rooms")
     public List<ChatRoomVo> findAllRoom() {
        return chatService.findAllRoom();
     }
