9c9f06f5c85e0ee89fd4e01cb0d6120c2404120e
q1awse-a <iltor91@gmail.com>
2025-05-30 13:13:25 +0900

    [공통] feat-jcm: 헤더 검색 기능 구현 중, 리스트 목록 수정 필요

--- 2_webProject/JobFinder/src/main/java/com/joblessfriend/jobfinder/search/dao/SearchDaoImpl.java
+++ 2_webProject/JobFinder/src/main/java/com/joblessfriend/jobfinder/search/dao/SearchDaoImpl.java
@@ -16,31 +16,20 @@ public class SearchDaoImpl implements SearchDao {
 	@Autowired
 	private SqlSession sqlSession;
 	
-	String namespace = "com.joblessfreind.jobfinder.search.";
+	String namespace = "com.joblessfriend.jobfinder.search.";
 	
 	
 	@Override
-	public int getRecruitmentTotalCount(SearchVo searchVo) {
+	public int getRecruitmentSearchTotalCount(SearchVo searchVo) {
 		// TODO Auto-generated method stub
-		return 0;
+		return sqlSession.selectOne(namespace + "getRecruitmentSearchTotalCount", searchVo);
 	}
 
 	@Override
-	public List<RecruitmentVo> recruitmentList(SearchVo searchVo) {
+	public List<RecruitmentVo> getRecruitmentSearchList(SearchVo searchVo) {
 		// TODO Auto-generated method stub
-		return null;
+		return sqlSession.selectList(namespace + "getRecruitmentSearchList", searchVo);
 	}
 
-	@Override
-	public int getCommunityTotalCount(SearchVo searchVo) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public List<CommunityVo> communityList(SearchVo searchVo) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
